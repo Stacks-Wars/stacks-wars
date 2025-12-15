da0d4f62edf7eb40ccc8fe847bfa4f86d0a9e8ce
Atomic <atomic.k.2739@gmail.com>
2025-12-15 16:44:22 +0100

    ✨ Feat(shared): introduce shared env-based app configuration constants

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,13 @@
+const DEV = process.env.NODE_ENV !== "production";
+export const DOMAIN_NAME = "stackswars.com";
+export const FRONTEND_LOCALHOST_PORT = 4002;
+export const FRONTEND_URL = DEV
+	? `http://localhost:${FRONTEND_LOCALHOST_PORT}`
+	: `https://www.${DOMAIN_NAME}`;
+
+export const BETTERAUTH_URL = DEV
+	? `http://localhost:${FRONTEND_LOCALHOST_PORT}`
+	: `https://www.${DOMAIN_NAME}`;
+
+// UTILS
+export const BNS_ONE_API_BASE_URL = "https://api.bns.one/";
