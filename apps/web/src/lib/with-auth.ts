3fbb9ad35bd0b8677ebbdb40b178ab62e7b720fc
Atomic <atomic.k.2739@gmail.com>
2025-12-15 20:25:02 +0100

    ✨ Feat(web): add helper code for @stacks-wars/auth in web

---
+++ apps/web/src/lib/with-auth.ts
@@ -0,0 +1,21 @@
+import { auth } from "@stacks-wars/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import type { JSX } from "react";
+import type { Session } from "./definitions/auth";
+
+// HOC for pages that require authentication
+export function withAuth<P extends { session: Session }>(
+	component: (props: P) => Promise<JSX.Element>,
+): (props: Omit<P, "session">) => Promise<JSX.Element> {
+	return async (props: Omit<P, "session">) => {
+		const headersList = await headers();
+		const session = await auth.api.getSession({ headers: headersList });
+
+		if (!session) {
+			redirect("/");
+		}
+
+		return component({ ...props, session } as unknown as P);
+	};
+}
