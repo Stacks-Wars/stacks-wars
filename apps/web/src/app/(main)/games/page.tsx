0574cb01f05db871bbe4b0c0745834645f12fb93
Flames <isaacomenuche@gmail.com>
2025-12-24 21:30:20 +0100

    feat(games): update GamesPage layout and enhance GameCard component with new style

--- apps/web/src/app/(main)/games/page.tsx
+++ apps/web/src/app/(main)/games/page.tsx
@@ -1,72 +1,24 @@
 import GameCard from "@/components/main/game-card";
 import { ApiClient } from "@/lib/api/client";
 import type { Game } from "@/lib/definitions";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default async function GamesPage() {
 	const games = await ApiClient.get<Game[]>("/api/games");
 
 	return (
-		<div className="container mx-auto">
-			{games.data?.map((game) => (
-				<GameCard {...game} />
-			))}
-			<div className="flex flex-col items-center">
-				<div className="flex justify-between items-center lg:gap-36 gap-10 bg-card lg:p-20 p-5 lg:rounded-xl rounded-[12px]">
-					<div>
-						<h3 className="lg:text-[40px]/10 text-[10px]/[10px] font-medium lg:mb-4 mb-1">
-							Lexi Wars
-						</h3>
-						<p className="lg:text-2xl/6 text-[6.5px]/[6.5px] lg:mb-7 mb-2">
-							A word battle game where players complete with
-							words.
-						</p>
-						<div className="flex lg:gap-4 gap-1 lg:mb-9 mb-2">
-							<span className="lg:text-xl/6 text-[6px] bg-foreground/10 rounded-full lg:py-4 py-1 lg:px-7 px-2">
-								Word
-							</span>
-							<span className="lg:text-xl/6 text-[6px] bg-foreground/10 rounded-full lg:py-4 py-1 lg:px-7 px-2">
-								Strategy
-							</span>
-							<span className="lg:text-xl/6 text-[6px] bg-foreground/10 rounded-full lg:py-4 py-1 lg:px-7 px-2">
-								Multiplayer
-							</span>
-						</div>
-						<div className="lg:text-xl text-[5px] flex gap-5">
-							<p>
-								<span className="font-medium ">
-									Active Rooms:
-								</span>{" "}
-								<span>3</span>
-							</p>
-							<p>
-								<span className="f ont-medium">Ratings:</span>{" "}
-								<span>4.5</span>
-							</p>
-							<p>
-								<span className="font-medium">Volume:</span>{" "}
-								<span>1K STX</span>
-							</p>
-						</div>
-					</div>
-					<Image
-						src={"/images/lexi-wars.svg"}
-						alt="lexi wars"
-						width={358}
-						height={182}
-						className="lg:w-89.5 w-24"
-						loading="lazy"
-					/>
-				</div>
-				<Button
-					className="-translate-y-1/2 w-full lg:max-w-80 max-w-24 lg:px-12 px-3 lg:py-6 py-1.5 rounded-full lg:text-xl text-[5px] font-medium"
-					asChild
-				>
-					<Link href={"/games"}>Play Now</Link>
-				</Button>
+		<div className="container mx-auto px-4">
+			<div className="py-4 lg:py-15">
+				<h1 className="text-2xl md:text-5xl font-bold text-center mb-2">
+					Available Games
+				</h1>
+				<p className="text-xs md:text-2xl font-medium text-center">
+					Choose from our selection of games to compete and win STX
+					rewards
+				</p>
 			</div>
+			{games.data?.map((game, i) => (
+				<GameCard key={i} {...game} />
+			))}
 		</div>
 	);
 }
--- apps/web/src/components/main/game-card.tsx
+++ apps/web/src/components/main/game-card.tsx
@@ -2,37 +2,39 @@ import Image from "next/image";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import type { Game } from "@/lib/definitions";
+import { IoStar } from "react-icons/io5";
 
 export default function GameCard(game: Game) {
 	return (
-		<div className="flex flex-col items-center">
-			<div className="flex justify-between items-center lg:gap-36 gap-10 bg-card lg:p-20 p-5 lg:rounded-xl rounded-[12px]">
+		<div className="flex flex-col items-center w-full">
+			<div className="flex flex-col-reverse sm:flex-row justify-between items-center gap-4 lg:gap-36 bg-card px-4 lg:px-17.5 py-10 lg:py-15.5 rounded-4xl w-full">
 				<div>
-					<h3 className="lg:text-[40px]/10 text-[10px]/[10px] font-medium lg:mb-4 mb-1">
+					<h3 className="md:text-[40px] text-2xl truncate font-bold pb-7">
 						{game.name}
 					</h3>
-					<p className="lg:text-2xl/6 text-[6.5px]/[6.5px] lg:mb-7 mb-2">
+					<p className="text-base truncate md:text-2xl sm:font-medium pb-4">
 						{game.description}
 					</p>
-					<div className="flex lg:gap-4 gap-1 lg:mb-9 mb-2">
+					<div className="flex flex-wrap gap-3 pb-4">
 						{game.category && (
-							<span className="lg:text-xl/6 text-[6px] bg-foreground/10 rounded-full lg:py-4 py-1 lg:px-7 px-2">
+							<span className="text-xs md:text-sm bg-foreground/10 rounded-full py-2 md:py-2.5 px-4 md:px-5 md:font-medium">
 								{game.category}
 							</span>
 						)}
 					</div>
-					<div className="lg:text-xl text-[5px] flex gap-5">
+					<div className="text-sm md:text-xl md:font-medium flex flex-wrap gap-4">
 						<p>
 							<span className="font-medium ">Active Rooms:</span>{" "}
 							<span>3</span>
 						</p>
 						<p>
-							<span className="f ont-medium">Ratings:</span>{" "}
+							<span className="font-medium">Ratings:</span>{" "}
 							<span>4.5</span>
 						</p>
-						<p>
+						<p className="flex items-center gap-1">
 							<span className="font-medium">Volume:</span>{" "}
-							<span>1K STX</span>
+							<span>1K STX</span>{" "}
+							<IoStar className="text-yellow-400" />
 						</p>
 					</div>
 				</div>
@@ -41,12 +43,12 @@ export default function GameCard(game: Game) {
 					alt="game logo"
 					width={358}
 					height={182}
-					className="lg:w-89.5 w-24"
 					loading="lazy"
+					className="w-40 md:w-89.5"
 				/>
 			</div>
 			<Button
-				className="-translate-y-1/2 w-full lg:max-w-80 max-w-24 lg:px-12 px-3 lg:py-6 py-1.5 rounded-full lg:text-xl text-[5px] font-medium"
+				className="-translate-y-1/2 w-full max-w-28 md:max-w-80 rounded-full text-xs md:text-xl sm:font-medium"
 				asChild
 			>
 				<Link href={`/games/${game.path}`}>Play Now</Link>
