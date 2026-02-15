4bee4b9f0a01049a2e443cc24c1c2cd10291ab15
Atomic <atomic.k.2739@gmail.com>
2026-02-15 16:01:22 +0100

    ✨ Feat(web): fetch games from api

--- apps/web/src/app/(main)/(landing)/page.tsx
+++ apps/web/src/app/(main)/(landing)/page.tsx
@@ -1,13 +1,18 @@
-import GamesShowcase from "./_components/games-showcase";
+import { Suspense } from "react";
+import GamesShowcase, {
+	GamesShowcaseSkeleton,
+} from "./_components/games-showcase";
 import Hero from "./_components/hero";
 import PlayToEarn from "./_components/play-to-earn";
 import TryOut from "./_components/try-out";
 
 export default function HomePage() {
 	return (
-		<div className="container mx-auto overflow-x-hidden! px-4 py-8">
+		<div className="container mx-auto overflow-x-hidden px-4 py-8">
 			<Hero />
-			<GamesShowcase />
+			<Suspense fallback={<GamesShowcaseSkeleton />}>
+				<GamesShowcase />
+			</Suspense>
 			<PlayToEarn />
 			<TryOut />
 		</div>
